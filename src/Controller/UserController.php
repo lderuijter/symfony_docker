8e4cb06d5ef9cd029d521710d99c3532e2793d9b
Lucas de Ruijter <lderuijter2@student.scalda.nl>
2023-07-11 16:42:47 +0200

    database created and docker installed

---
+++ src/Controller/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route('/create', name: 'create_user')]
+    public function createUser(EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $user->setName('123');
+        $user->setPassword('123');
+
+        // tell Doctrine you want to (eventually) save the User (no queries yet)
+        $entityManager->persist($user);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new user with id '.$user->getId());
+    }
+}
