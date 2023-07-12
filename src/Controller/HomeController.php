0fa9ae7db96e2136780348cb50e2e9f3876ce00e
Lucas de Ruijter <lderuijter2@student.scalda.nl>
2023-07-12 23:14:41 +0200

    login and logout added with authentication in the toolbar :)

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
+class HomeController extends AbstractController
+{
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        $user = $this->security->getUser();
+        if($user != null) {
+            $username = $user->getUsername();
+        } else {
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render('home/index.html.twig', [
+            'username' => $username,
+        ]);
+    }
+}
