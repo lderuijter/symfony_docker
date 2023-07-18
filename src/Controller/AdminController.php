474647036d5fd9160cb879d5770fb908639f3716
lderuijter <lderuijter2@student.scalda.nl>
2023-07-18 21:36:00 +0200

    delete user method

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,17 +5,28 @@ namespace App\Controller;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntityManagerInterface $entityManager, Request $request): Response
     {
         $users = $entityManager->getRepository(User::class)->findAll();
+
         return $this->render('admin/index.html.twig', [
             'users' => $users,
         ]);
+    }
+    #[Route('/admin/deleteUser/{userId}', name: 'app_admin_deleteUser')]
+    public function deleteUser(int $userId, EntityManagerInterface $entityManager): Response
+    {
+        $user = $entityManager->getRepository(User::class)->find($userId);
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_admin');
     }
 }
